bf20a0ec22c6e6723ec149c37946c031969fcf43
jordansali <43249323+jordansali@users.noreply.github.com>
2020-03-11 15:27:06 -0400

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,8 @@
 import React from "react"
 
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
