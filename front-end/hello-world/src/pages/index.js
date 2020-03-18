80d33da5fe20fdae81c891025878e73253925f16
Daniel Spagnuolo <daniel.spagnuolo44@gmail.com>
2020-03-18 09:45:13 -0400

    Revert "Test of change to file"
    This reverts commit f402c3d10afaa2894062dd11b0123e2a324df2b5.

--- front-end/hello-world/src/pages/index.js
+++ front-end/hello-world/src/pages/index.js
@@ -1,3 +1,3 @@
 import React from "react"
 
-export default () => <div>Hello world! People...! This is a test site built using Gatsby. And here is a test change!</div>
+export default () => <div>Hello world! People...! This is a test site built using Gatsby.</div>
