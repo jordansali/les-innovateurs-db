58dd0cec2d5962e922978b1a8e5e42c2f6cb67ba
Daniel Spagnuolo <daniel.spagnuolo44@gmail.com>
2020-03-31 12:32:03 -0400

    Added feature as per pbi#78

--- front-end/hello-world/src/pages/index.js
+++ front-end/hello-world/src/pages/index.js
@@ -4,5 +4,6 @@ export default () => (
     <div>
         <h1>Testing branch!</h1>
         <div>Hello world!!! People...! This is a test site built using Gatsby. Test of change</div>
+        <div>This is a new feature!</div>
     </div>
 )
