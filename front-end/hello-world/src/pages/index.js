abe2a55f3b0e1fe560e646844ec3a1633c668689
jordansali <jordan.sali@canada.ca>
2020-03-31 11:02:28 -0400

    restored index.js file to its natural glory

--- front-end/hello-world/src/pages/index.js
+++ front-end/hello-world/src/pages/index.js
@@ -1,7 +1,3 @@
 import React from "react"
 
-<<<<<<< HEAD
-export default () => <div>Hello world! </div>
-=======
 export default () => <div>Hello world! People...! This is a test site built using Gatsby. Test of change</div>
->>>>>>> 2dd106d8cd32b8ec7c9449e5c11d97c2a53e097c
