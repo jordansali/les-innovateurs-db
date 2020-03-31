c0096565a3cee01a9f06187722d9d113a64a7c8d
Daniel Spagnuolo <daniel.spagnuolo44@gmail.com>
2020-03-31 12:15:00 -0400

    Reformed index, added test line

--- front-end/hello-world/src/pages/index.js
+++ front-end/hello-world/src/pages/index.js
@@ -1,3 +1,8 @@
 import React from "react"
 
-export default () => <div>Hello world! People...! This is a test site built using Gatsby. Test of change</div>
+export default () => (
+    <div>
+        <h1>Testing branch!</h1>
+        <div>Hello world! People...! This is a test site built using Gatsby. Test of change</div>
+    </div>
+)
