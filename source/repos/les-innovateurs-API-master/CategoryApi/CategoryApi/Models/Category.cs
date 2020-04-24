5828dad074b6900b72d5324b00d31e44bfc85b8f
Sanderson91 <Sanderson91@users.noreply.github.com>
2020-04-24 08:46:51 -0400

    updated status codes

---
+++ source/repos/les-innovateurs-API-master/CategoryApi/CategoryApi/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CategoryApi.Models
+{
+    public partial class Category
+    {
+        public Category()
+        {
+            Questions = new HashSet<Questions>();
+        }
+
+        public int CategoryId { get; set; }
+        public string TitleEn { get; set; }
+        public string TitleFr { get; set; }
+
+        public virtual ICollection<Questions> Questions { get; set; }
+    }
+}
---
+++ source/repos/les-innovateurs-API-master/CategoryApi/CategoryApi/Models/Questions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CategoryApi.Models
+{
+    public partial class Questions
+    {
+        public int QuestionId { get; set; }
+        public int? CategoryId { get; set; }
+        public string TextEn { get; set; }
+        public string TextFr { get; set; }
+        public string DifficultyVal { get; set; }
+        public string AnswerEn { get; set; }
+        public string AnswerFr { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
